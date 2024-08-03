eec4519c3947fd9c7062b4a41107e34383780f15
ayPromise servant <fatherpother@gmail.com>
2024-08-04 02:16:39 +0300

    button v0.2.0

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { ComponentProps, ElementType } from 'react';
 import clsx from 'clsx';
-import Loader from './Loader';
+import Loader from './components/Loader';
+import { buttonVariants, ButtonVariants } from './styles/Button.variants';
 
-export type ButtonProps = {
-  type?: 'button' | 'submit' | 'reset' | 'menu'
+export type ButtonCustomProps<E extends ElementType = ElementType> = {
+  clickAnimation?: boolean;
+  loading?: boolean;
+  sideOfLoader?: 'left' | 'centerAlone' | 'right';
+  loadingText?: string;
+  customLoader?: React.ReactNode;
+  fullWidth?: boolean;
+  disabled?: boolean;
+  children?: React.ReactNode;
+  className?: string;
+  as?: E;
+} & ButtonVariants & {
   variant?: 'solid' | 'outline' | 'link';
   size?: 'sm' | 'md' | 'lg';
   color?: 'blue' | 'gray' | 'red' | 'green' | 'purple' | 'orange' | 'teal' | 'pink' | 'indigo' | 'yellow';
   transition?: 'smooth' | 'quick' | 'none';
   hoverEffect?: 'scale' | 'none';
   clickAnimation?: boolean;
-  disabled?: boolean;
-  loading?: boolean;
-  sideOfLoader?: 'left' | 'centerAlone' | 'right';
-  loadingText?: string;
-  customLoader?: React.ReactNode;
-  fullWidth?: boolean;
-  className?: string;
-  children: React.ReactNode;
-  onClick?: Function;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+};
 
-const Button: React.FC<ButtonProps> = ({
-  type = 'button',
+interface ButtonWithForwardRef extends React.FC<ButtonCustomProps<ElementType>> {
+  <E extends ElementType>(props: ButtonCustomProps<E> & { ref?: React.Ref<any> }): ReturnType<React.FC<ButtonCustomProps<E>>>
+}
+
+type ButtonProps<E extends ElementType> = ButtonCustomProps<E> & Omit<ComponentProps<E>, keyof ButtonCustomProps>
+
+
+const defaultElementType: ElementType = "button"
+
+const Button: ButtonWithForwardRef = <E extends ElementType = typeof defaultElementType>({
   variant = 'solid',
   size = 'md',
   color = 'blue',
@@ -37,115 +47,27 @@ const Button: React.FC<ButtonProps> = ({
   fullWidth = false,
   className,
   children,
-  ...props
-}) => {
-  const baseStyles = 'flex px-4 py-2 items-center justify-center rounded';
-
-  const colorScheme = {
-    blue: {
-      solid: 'bg-blue-solid text-blue-text hover:bg-blue-hover',
-      outline: 'border border-blue-solid text-blue-solid hover:bg-blue-hover hover:text-blue-text',
-      link: 'text-blue-solid hover:text-blue-hover',
-    },
-    gray: {
-      solid: 'bg-gray-solid text-gray-text hover:bg-gray-hover',
-      outline: 'border border-gray-solid text-gray-solid hover:bg-gray-hover hover:text-gray-text',
-      link: 'text-gray-solid hover:text-gray-hover',
-    },
-    red: {
-      solid: 'bg-red-solid text-red-text hover:bg-red-hover',
-      outline: 'border border-red-solid text-red-solid hover:bg-red-hover hover:text-red-text',
-      link: 'text-red-solid hover:text-red-hover',
-    },
-    green: {
-      solid: 'bg-green-solid text-green-text hover:bg-green-hover',
-      outline: 'border border-green-solid text-green-solid hover:bg-green-hover hover:text-green-text',
-      link: 'text-green-solid hover:text-green-hover',
-    },
-    purple: {
-      solid: 'bg-purple-solid text-purple-text hover:bg-purple-hover',
-      outline: 'border border-purple-solid text-purple-solid hover:bg-purple-hover hover:text-purple-text',
-      link: 'text-purple-solid hover:text-purple-hover',
-    },
-    orange: {
-      solid: 'bg-orange-solid text-orange-text hover:bg-orange-hover',
-      outline: 'border border-orange-solid text-orange-solid hover:bg-orange-hover hover:text-orange-text',
-      link: 'text-orange-solid hover:text-orange-hover',
-    },
-    teal: {
-      solid: 'bg-teal-solid text-teal-text hover:bg-teal-hover',
-      outline: 'border border-teal-solid text-teal-solid hover:bg-teal-hover hover:text-teal-text',
-      link: 'text-teal-solid hover:text-teal-hover',
-    },
-    pink: {
-      solid: 'bg-pink-solid text-pink-text hover:bg-pink-hover',
-      outline: 'border border-pink-solid text-pink-solid hover:bg-pink-hover hover:text-pink-text',
-      link: 'text-pink-solid hover:text-pink-hover',
-    },
-    indigo: {
-      solid: 'bg-indigo-solid text-indigo-text hover:bg-indigo-hover',
-      outline: 'border border-indigo-solid text-indigo-solid hover:bg-indigo-hover hover:text-indigo-text',
-      link: 'text-indigo-solid hover:text-indigo-hover',
-    },
-    yellow: {
-      solid: 'bg-yellow-solid text-yellow-text hover:bg-yellow-hover',
-      outline: 'border border-yellow-solid text-yellow-solid hover:bg-yellow-hover hover:text-yellow-text',
-      link: 'text-yellow-solid hover:text-yellow-hover',
-    },
-  };
+  as: Component = defaultElementType as E,
+  ...otherProps
+}: ButtonProps<E>) => {
+  const buttonStyles = buttonVariants({ variant: variant, size: size, color: color, transition: transition, hoverEffect: hoverEffect, clickAnimation: clickAnimation, disabled: disabled || loading })
 
 
-
-  const sizeStyles = {
-    sm: 'w-[120px] h-[30px] text-sm',
-    md: 'w-[150px] h-[40px] text-base',
-    lg: 'w-[180px] h-[50px] text-lg',
-  };
-
-  const transitionStyles = {
-    smooth: "transition-all duration-500",
-    quick: "transition-all duration-100",
-    none: ""
-  }
-
-  const hoverEffectStyles = {
-    scale: "hover:scale-95",
-    none: ""
-  }
-
-  const animationStyles = 'active:animate-click'
-  const disabledStyles = 'opacity-50 cursor-not-allowed hover pointer-events-none'
-
-  const buttonStyles = clsx(
-    baseStyles,
-    colorScheme[color][variant],
-    sizeStyles[size as keyof typeof sizeStyles],
-    transitionStyles[transition as keyof typeof transitionStyles],
-    hoverEffectStyles[hoverEffect as keyof typeof hoverEffectStyles],
-    {
-      [animationStyles]: clickAnimation,
-      [disabledStyles]: disabled || loading,
-      'w-full': fullWidth
-    },
-    className
+  const defaultLoaderColor = "border" + buttonStyles.split(" ").find(part => part.startsWith("text-") && part !== "text-base")?.slice(4)
+  const loaderElement = (
+    <div className={clsx("flex justify-between items-center gap-2", {
+      "flex-row-reverse": sideOfLoader === "right"
+    })}>
+      {customLoader || <Loader color={defaultLoaderColor} />}
+      {sideOfLoader !== "centerAlone" && loadingText}
+    </div>
   );
 
-  const deafultLoaderColor = "border" + buttonStyles.split(" ").find(part => part.startsWith("text-") && part !== "text-base")?.slice(4)
-
   return (
-    <button
-      type={type}
-      className={buttonStyles}
-      disabled={disabled || loading}
-      {...props}
-    >
-      {loading ? <div className={clsx("flex justify-between items-center gap-2", {
-        "flex-row-reverse": sideOfLoader === "right"
-      })}>
-        {customLoader || <Loader color={deafultLoaderColor}></Loader>}
-        {sideOfLoader !== "centerAlone" && loadingText}
-      </div> : children}
-    </button>
+    //@ts-ignore
+    <Component className={`${buttonStyles} ${className}`} {...otherProps}>
+      {loading ? loaderElement : children}
+    </Component>
   );
 };
 
---
+++ components/Button/styles/Button.variants.ts
@@ -0,0 +1,98 @@
+import { cva, VariantProps } from "class-variance-authority";
+import styles from "./Button.module.css"
+
+export const buttonVariants = cva(
+  styles.base,
+  {
+    variants: {
+      color: {
+        blue: '',
+        gray: '',
+        red: '',
+        green: '',
+        purple: '',
+        orange: '',
+        teal: '',
+        pink: '',
+        indigo: '',
+        yellow: '',
+      },
+      variant: {
+        solid: '',
+        outline: 'border',
+        link: '',
+      },
+      size: {
+        sm: styles.sizeSm,
+        md: styles.sizeMd,
+        lg: styles.sizeLg,
+      },
+      transition: {
+        smooth: styles.transitionSmooth,
+        quick: styles.transitionQuick,
+        none: '',
+      },
+      hoverEffect: {
+        scale: styles.hoverScale,
+        none: '',
+      },
+      clickAnimation:{
+        true:styles.clickAnimation,
+        false:""
+      },
+      disabled:{
+        true:styles.disabled,
+        false:""
+      }
+    },
+    compoundVariants: [
+        { variant: 'solid', color: 'blue', className: styles.SolidBlue },
+        { variant: 'outline', color: 'blue', className: styles.OutlineBlue },
+        { variant: 'link', color: 'blue', className: styles.LinkBlue },
+        
+        { variant: 'solid', color: 'gray', className: styles.SolidGray },
+        { variant: 'outline', color: 'gray', className: styles.OutlineGray },
+        { variant: 'link', color: 'gray', className: styles.LinkGray },
+
+        { variant: 'solid', color: 'red', className: styles.SolidRed },
+        { variant: 'outline', color: 'red', className: styles.OutlineRed },
+        { variant: 'link', color: 'red', className: styles.LinkRed },
+
+        { variant: 'solid', color: 'green', className: styles.SolidGreen },
+        { variant: 'outline', color: 'green', className: styles.OutlineGreen },
+        { variant: 'link', color: 'green', className: styles.LinkGreen },
+
+        { variant: 'solid', color: 'purple', className: styles.SolidPurple },
+        { variant: 'outline', color: 'purple', className: styles.OutlinePurple },
+        { variant: 'link', color: 'purple', className: styles.LinkPurple },
+
+        { variant: 'solid', color: 'orange', className: styles.SolidOrange },
+        { variant: 'outline', color: 'orange', className: styles.OutlineOrange },
+        { variant: 'link', color: 'orange', className: styles.LinkOrange },
+
+        { variant: 'solid', color: 'teal', className: styles.SolidTeal },
+        { variant: 'outline', color: 'teal', className: styles.OutlineTeal },
+        { variant: 'link', color: 'teal', className: styles.LinkTeal },
+
+        { variant: 'solid', color: 'pink', className: styles.SolidPink },
+        { variant: 'outline', color: 'pink', className: styles.OutlinePink },
+        { variant: 'link', color: 'pink', className: styles.LinkPink },
+
+        { variant: 'solid', color: 'indigo', className: styles.SolidIndigo },
+        { variant: 'outline', color: 'indigo', className: styles.OutlineIndigo },
+        { variant: 'link', color: 'indigo', className: styles.LinkIndigo },
+
+        { variant: 'solid', color: 'yellow', className: styles.SolidYellow },
+        { variant: 'outline', color: 'yellow', className: styles.OutlineYellow },
+        { variant: 'link', color: 'yellow', className: styles.LinkYellow },],
+    defaultVariants: {
+      variant: 'solid',
+      size: 'md',
+      color: 'blue',
+      transition: 'quick',
+      hoverEffect: 'scale',
+    },
+  }
+);
+
+export type ButtonVariants = VariantProps<typeof buttonVariants>
