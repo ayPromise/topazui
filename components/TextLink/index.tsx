0edbeb084c5ca19cff87f46c6e4eaa252f46405c
ayPromise servant <fatherpother@gmail.com>
2024-08-08 21:46:36 +0300

    textlink v0.1.1

--- components/TextLink/index.tsx
+++ components/TextLink/index.tsx
@@ -1,16 +1,21 @@
 import { ChildrenAndStyles } from '@/types'
+import Link from 'next/link'
 import React, { AllHTMLAttributes, ComponentProps } from 'react'
 import { tv } from 'tailwind-variants'
 
 type TextLinkProps = ChildrenAndStyles & AllHTMLAttributes<HTMLAnchorElement>
 
-const TextLink = ({ className, ...props }: TextLinkProps) => {
+const TextLink = ({ className, href, ...props }: TextLinkProps) => {
+    const isStorybook = process.env.STORYBOOK === 'true';
+
     const linkStyles = tv({
-        base: 'text-blue-600 hover:underline'
+        base: 'text-blue-600 hover:underline cursor-pointer'
     })
-    return (
-        <a className={linkStyles({ className })} {...props} />
-    )
+    if (isStorybook)
+        return <a className={linkStyles({ className })} {...props} />
+    else
+        return <Link href={href || ''} className={linkStyles({ className })} {...props} />
+
 }
 
 export default TextLink
